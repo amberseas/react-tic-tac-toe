acef8dc7b3a2eaa854aaaa77f830e403ba4b1c44
Gintare Simkuniene <gintareba@gmail.com>
2025-06-03 15:19:58 +0300

    add player

---
+++ src/components/Player.jsx
@@ -0,0 +1,24 @@
+import {useState} from "react";
+
+export default function Player ({name, symbol}) {
+    const [playerName, setPlayerName] = useState(name);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleEdit = () => {
+        setIsEditing(prev => !prev);
+    };
+
+    const handleNameEdit = (e) => {
+        setPlayerName(e.target.value);
+    };
+    return (
+        <li>
+            <span className="player">
+                {!isEditing && <span className="player-name">{playerName}</span>}
+                {isEditing && <input type='text' className="player-name" value={playerName} onChange={handleNameEdit} required />}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEdit}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
+    );
+}
